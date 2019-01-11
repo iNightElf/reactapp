f481d4f50c7e0e28ee9fd8ad254405363f50528d
Shahzad <hope_charm_angel@yahoo.com>
2019-01-11 10:50:15 +0600

    Updates

---
+++ precache-manifest.c7c6f6a99f6fced1a4f4317349b88c42.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "66de5dd7d2a28560e0e5",
+    "url": "/reactapp/static/css/main.6f96fbc0.chunk.css"
+  },
+  {
+    "revision": "66de5dd7d2a28560e0e5",
+    "url": "/reactapp/static/js/main.66de5dd7.chunk.js"
+  },
+  {
+    "revision": "3d86bb7d7bcf8f7cadeb",
+    "url": "/reactapp/static/js/1.3d86bb7d.chunk.js"
+  },
+  {
+    "revision": "75024734ab9c32c58e65",
+    "url": "/reactapp/static/js/runtime~main.75024734.js"
+  },
+  {
+    "revision": "f0ab4e5ec26a805dc6db997a8898993b",
+    "url": "/reactapp/static/media/slide_1.f0ab4e5e.jpg"
+  },
+  {
+    "revision": "839dd41bfb86393062c5b20975841877",
+    "url": "/reactapp/static/media/slide_2.839dd41b.jpg"
+  },
+  {
+    "revision": "a839a474fc7bb4bbd72101d571fd09db",
+    "url": "/reactapp/static/media/slide_3.a839a474.jpg"
+  },
+  {
+    "revision": "dc855a2c605c91df406d440585969de6",
+    "url": "/reactapp/static/media/slide_4.dc855a2c.jpg"
+  },
+  {
+    "revision": "877822b3aabe7261f5ff2fa133a8d11b",
+    "url": "/reactapp/index.html"
+  }
+];
